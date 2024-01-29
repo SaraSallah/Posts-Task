3432307df138c5e9d972f76a1a652d748421dc27
Sara Salah <93276124+SaraSallah@users.noreply.github.com>
2024-01-29 12:14:45 +0200

    add data model

---
+++ app/src/main/java/com/example/posts_task/data/remote/model/Post.kt
@@ -0,0 +1,15 @@
+package com.example.posts_task.data.remote.model
+
+
+import com.google.gson.annotations.SerializedName
+
+data class Post(
+    @SerializedName("body")
+    val body: String? = null,
+    @SerializedName("id")
+    val id: Int? = null,
+    @SerializedName("title")
+    val title: String? = null,
+    @SerializedName("userId")
+    val userId: Int? = null
+)
